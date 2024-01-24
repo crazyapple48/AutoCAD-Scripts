15892b875b64b5e6e22791a43800dc5beeec25b3
Kevin Penner <111170452+crazyapple48@users.noreply.github.com>
2024-01-24 16:55:40 -0700

    Fixed Titleblock size

--- tb.py
+++ tb.py
@@ -8,7 +8,7 @@ layout1 = acad.doc.PaperSpace
 
 DESIGNER = "Douglas Clark"
 DIRECTOR = "Anthony Daniels"
-SHOW = "Charlie and the Chocolate Factory"
+SHOW = "Charlie and the\nChocolate Factory"
 SCALE = '1/2" = 1\''
 CURRENTDATE = datetime.now()
 CURRENTMONTH = CURRENTDATE.month
@@ -20,19 +20,24 @@ PROJECT = input("Enter Project Name: ")
 PLATE = input("Enter Plate Title: ")
 PAGE_NUMBER = input("Enter Page Number: ")
 
-p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20 = [APoint(0.15, 0.15), APoint(0.15, 10.85),
-                                                                                             APoint(16.85, 10.85), APoint(
-    16.85, 0.15),
-    APoint(0.15, 1.65), APoint(
-    16.85, 1.65),
-    APoint(3.4, 1.65), APoint(
-    3.4, 0.15),
-    APoint(7.4, 1.65), APoint(7.4, 0.15),
-    APoint(11.65, 1.65), APoint(11.65, 0.15),
-    APoint(15.65, 1.65), APoint(15.65, 0.15),
-    APoint(7.4, 0.65), APoint(15.65, 0.65),
-    APoint(7.4, 1.15), APoint(15.65, 1.15),
-    APoint(0.15, 0.9), APoint(3.4, 0.9)]
+p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20 = [APoint(0.375, 0.375), APoint(0.375, 10.625),
+                                                                                             APoint(16.625, 10.625), APoint(
+                                                                                                 16.625, 0.375),
+                                                                                             APoint(0.375, 1.375), APoint(
+                                                                                                 16.625, 1.375),
+                                                                                             APoint(3.4, 1.375), APoint(
+                                                                                                 3.4, 0.375),
+                                                                                             APoint(7.4, 1.375), APoint(
+                                                                                                 7.4, 0.375),
+                                                                                             APoint(11.65, 1.375), APoint(
+                                                                                                 11.65, 0.375),
+                                                                                             APoint(15.65, 1.375), APoint(
+                                                                                                 15.65, 0.375),
+                                                                                             APoint(7.4, 0.708), APoint(
+                                                                                                 15.65, 0.708),
+                                                                                             APoint(7.4, 1.041), APoint(
+                                                                                                 15.65, 1.041),
+                                                                                             APoint(0.375, 0.875), APoint(3.4, 0.875)]
 
 if PAGE_NUMBER == "1":
     trcorner = APoint(p2[0] + 3, p2[1])
@@ -63,46 +68,46 @@ line11 = layout1.AddLine(p17, p18)
 line12 = layout1.AddLine(p19, p20)
 
 # Add Static Text
-show = layout1.AddMText(APoint(.5, 1.5), 3.25, f'{SHOW}')
-show.Height = 0.175
+show = layout1.AddMText(APoint(.5, 1.3), 3.25, f'{SHOW}')
+show.Height = 0.125
 acad.doc.SendCommand("JUSTIFYTEXT " + "last" + "\n" +
                      " " + "\n" + "middle center ")
 
-plate = layout1.AddMText(APoint(.5, .75), 3.25, f'{
+plate = layout1.AddMText(APoint(.5, .8), 3.25, f'{
                          PROJECT}' + "\n" + f'{PLATE}')
-plate.Height = 0.175
+plate.Height = 0.125
 acad.doc.SendCommand("JUSTIFYTEXT " + f'{plate}' + "\n" +
                      " " + "\n" + "middle center ")
 
-abt = layout1.AddMText(APoint(4.55, 1.5), 3.75, "Arizona\nBroadway\nTheatre")
-abt.Height = 0.25
+abt = layout1.AddMText(APoint(4.55, 1.3), 3.75, "Arizona\nBroadway\nTheatre")
+abt.Height = 0.2
 acad.doc.SendCommand("JUSTIFYTEXT " + "last" + "\n" +
                      " " + "\n" + "middle center ")
 
 directedBy = layout1.AddMText(
-    APoint(7.6, 1.5), 4.25, f'Directed By: {DIRECTOR}')
-directedBy.Height = 0.175
+    APoint(7.6, 1.3), 4.25, f'Directed By: {DIRECTOR}')
+directedBy.Height = 0.125
 
 
 designedBy = layout1.AddMText(
-    APoint(11.8, 1.5), 4.25, f'Designed By: {DESIGNER}')
-designedBy.Height = 0.175
+    APoint(11.8, 1.3), 4.25, f'Designed By: {DESIGNER}')
+designedBy.Height = 0.125
 
 
 drawn_by = layout1.AddMText(
-    APoint(7.6, 1), 4.25, "Drawn By: Kevin Penner")
-drawn_by.Height = 0.175
+    APoint(7.6, .95), 4.25, "Drawn By: Kevin Penner")
+drawn_by.Height = 0.125
 
 
-sm = layout1.AddMText(APoint(11.8, 1), 4.25, "SM: Nico Rossetti")
-sm.Height = 0.175
+sm = layout1.AddMText(APoint(11.8, .95), 4.25, "SM: Nico Rossetti")
+sm.Height = 0.125
 
-date = layout1.AddMText(APoint(7.6, .5), 4.25, f'Date: {DATE}')
-date.Height = 0.175
+date = layout1.AddMText(APoint(7.6, .65), 4.25, f'Date: {DATE}')
+date.Height = 0.125
 
 
-scale = layout1.AddMText(APoint(11.8, .5), 4.25, f'Scale: {SCALE}')
-scale.Height = 0.175
+scale = layout1.AddMText(APoint(11.8, .65), 4.25, f'Scale: {SCALE}')
+scale.Height = 0.125
 
-pageNumber = layout1.AddMText(APoint(16.15, 1.15), 1, f'{PAGE_NUMBER}')
+pageNumber = layout1.AddMText(APoint(16, 1.15), 1, f'{PAGE_NUMBER}')
 pageNumber.Height = 0.5
